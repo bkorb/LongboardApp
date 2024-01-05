c9c0c631ed01f23c846418cc9968d9b6f6fce200
Brandon Korb <bkorb@wm.edu>
2024-01-04 21:33:25 -0500

    First App Version

--- app/src/main/java/com/example/longboardapp/MainActivity.kt
+++ app/src/main/java/com/example/longboardapp/MainActivity.kt
@@ -4,35 +4,49 @@ import Message
 import Target
 import Values
 import android.icu.text.DecimalFormat
-import android.icu.text.NumberFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.longboardapp.ui.theme.LongboardAppTheme
 import encodeMessage
-import kotlinx.coroutines.*
 import okhttp3.Request
 import okhttp3.WebSocketListener
-import java.math.RoundingMode
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.round
 
@@ -50,11 +64,39 @@ class MainActivity : ComponentActivity() {
         val viewModel: SocketViewModel by viewModels()
         webSocketListener = WebSocketListener(viewModel)
         setContent {
+            var offsetX by remember { mutableStateOf(0f) }
+            var offsetY by remember { mutableStateOf(0f) }
             LongboardAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pointerInput(Unit) {
+                            detectDragGestures(onDragEnd = {
+                                offsetX = 0f
+                                offsetY = 0f
+                                val message = encodeMessage(
+                                    Message(
+                                        id = "setTarget",
+                                        fields = Target(RPMToERPM(MPHToRPM(0f)).toInt())
+                                    )
+                                ) ?: "Hello World!"
+                                viewModel.webSocket?.send(message)
+                            }) { change, dragAmount ->
+                                change.consume()
+                                offsetX += dragAmount.x
+                                offsetY += dragAmount.y
+                                val message = encodeMessage(
+                                    Message(
+                                        id = "setTarget",
+                                        fields = Target(RPMToERPM(MPHToRPM(offsetY*30f/size.height)).toInt())
+                                    )
+                                ) ?: "Hello World!"
+                                viewModel.webSocket?.send(message)
+                            }
+                    },
+                    color = MaterialTheme.colorScheme.background,
+
                 ) {
                     SocketContent(socketViewModel = viewModel, webSocketListener = webSocketListener)
                 }
@@ -65,77 +107,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun DataContent(text: String,
+                modifier: Modifier,
+                textColor: Color = Color.Black,
+                style: TextStyle = MaterialTheme.typography.bodyMedium,
+                textModifier: Modifier = Modifier,
+                shape: Shape = RectangleShape,
+                borderStroke: BorderStroke = BorderStroke(1.dp, Color.Black),
+                innerPaddingValues: PaddingValues = PaddingValues(0.dp),
+                outerPaddingValues: PaddingValues = PaddingValues(0.dp),
+                textAlign: TextAlign? = null,
+){
+    Box(modifier = modifier.padding(outerPaddingValues)) {
+        Text(
+            text = text,
+            modifier = textModifier
+                .fillMaxWidth()
+                .clip(shape)
+                .border(borderStroke, shape)
+                .padding(innerPaddingValues),
+            style = style,
+            color = textColor,
+            textAlign = textAlign
+        )
+    }
+}
+
+@Composable
+fun DataFillContent(text: String,
+                    color: Color,
+                    percent: Float,
+                    modifier: Modifier,
+                    textModifier: Modifier = Modifier,
+                    textColor: Color = Color.Black,
+                    style: TextStyle = MaterialTheme.typography.bodyMedium,
+                    innerPaddingValues: PaddingValues = PaddingValues(0.dp),
+                    outerPaddingValues: PaddingValues = PaddingValues(0.dp),
+                    shape: Shape = RectangleShape,
+                    borderStroke: BorderStroke = BorderStroke(1.dp, Color.Black),
+                    textAlign: TextAlign? = null,
+){
+    Box(modifier = modifier.padding(outerPaddingValues)) {
+        Text(
+            text = text,
+            color = textColor,
+            modifier = textModifier
+                .fillMaxWidth()
+                .clip(shape)
+                .drawBehind {
+                    val canvasWidth = size.width
+                    val canvasHeight = size.height
+                    drawRect(
+                        color = color,
+                        size = Size(
+                            max(canvasWidth * percent, canvasWidth * 0.1f),
+                            canvasHeight
+                        )
+                    )
+                }
+                .border(borderStroke, shape)
+                .padding(innerPaddingValues),
+            style = style,
+            textAlign = textAlign
+        )
+    }
+}
+
+@Composable
 fun SocketContent(socketViewModel: SocketViewModel = viewModel(), webSocketListener: WebSocketListener) {
     val rpm_formatter: DecimalFormat = DecimalFormat.getInstance() as DecimalFormat
     rpm_formatter.minimumFractionDigits = 0
     rpm_formatter.maximumFractionDigits = 0
     rpm_formatter.minimumIntegerDigits = 1
     rpm_formatter.maximumIntegerDigits = 5
+    val tach_formatter: DecimalFormat = DecimalFormat.getInstance() as DecimalFormat
+    tach_formatter.minimumSignificantDigits = 3
+    tach_formatter.maximumSignificantDigits = 3
     val battery_formatter: DecimalFormat = DecimalFormat.getInstance() as DecimalFormat
     battery_formatter.minimumFractionDigits = 0
     battery_formatter.maximumFractionDigits = 0
-    Column(modifier = Modifier.padding(16.dp)) {
-        Row(modifier = Modifier.padding(0.dp)) {
-            val dataModifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .background(Color.LightGray)
-            Text(
-                text = "M1: ${rpm_formatter.format(10*round(0.1*ERPMToRPM(socketViewModel.values1.rpm ?: 0f)))} RPM",
-                modifier = dataModifier.padding(16.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = "M2: ${rpm_formatter.format(10*round(0.1*ERPMToRPM(socketViewModel.values2.rpm ?: 0f)))} RPM",
-                modifier = dataModifier.padding(16.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = "Battery: ${battery_formatter.format(socketViewModel.batteryPercent*100)}%",
-                modifier = dataModifier
-                    .drawBehind {
-                        val canvasWidth = size.width
-                        val canvasHeight = size.height
-                        val battery_percent = socketViewModel.batteryPercent
-                        drawRect(
-                            color = Color(
-                                red = 1 - battery_percent,
-                                green = battery_percent,
-                                blue = 0f
-                            ),
-                            size = Size(
-                                max(canvasWidth * battery_percent, canvasWidth * 0.1f),
-                                canvasHeight
-                            )
-                        )
-                    }
-                    .padding(16.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-        Row(modifier = Modifier.padding(0.dp)) {
-            val dataModifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .background(Color.LightGray)
-            Text(
-                text = "M1: ${rpm_formatter.format(RPMToMPH(ERPMToRPM(socketViewModel.values1.rpm ?: 0f)))} MPH",
-                modifier = dataModifier.padding(16.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = "M1: ${rpm_formatter.format(RPMToMPH(ERPMToRPM(socketViewModel.values2.rpm ?: 0f)))} MPH",
-                modifier = dataModifier.padding(16.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
+    Column(modifier = Modifier.padding(16.dp, 4.dp)) {
         Column(modifier = Modifier
-            .padding(4.dp)
-            .height(200.dp)) {
+            .padding(0.dp, 2.dp)
+            .height(67.dp)) {
             val buttonModifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .padding(4.dp)
+                .padding(0.dp, 4.dp)
             Button(
                 onClick = {
                     if (socketViewModel.status) {
@@ -150,35 +208,125 @@ fun SocketContent(socketViewModel: SocketViewModel = viewModel(), webSocketListe
                         )
                     }
                 },
-                modifier = buttonModifier
+                modifier = buttonModifier,
+                shape = RoundedCornerShape(16.dp)
             ) {
                 Text(text = if (socketViewModel.status) "Disconnect" else "Connect")
             }
-            Button(
-                onClick = {
-                    val message = encodeMessage(
-                        Message(
-                            id = "setTarget",
-                            fields = Target(RPMTOERPM(MPHToRPM(25f)).toInt())
-                        )
-                    ) ?: "Hello World!"
-                    socketViewModel.webSocket?.send(message)
-                },
-                enabled = socketViewModel.status,
-                modifier = buttonModifier
-            ) {
-                Text(text = "Start")
+        }
+        Column(modifier = Modifier
+            .padding(0.dp, 2.dp)
+            .clip(shape = RoundedCornerShape(16.dp))
+            .background(Color.LightGray)
+        ) {
+            Row(modifier = Modifier.padding(0.dp)) {
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                val percent = socketViewModel.batteryPercent
+                DataFillContent(
+                    text = "Battery: ${battery_formatter.format(percent * 100)}%",
+                    color = Color(
+                        red = 1 - percent,
+                        green = percent,
+                        blue = 0f
+                    ),
+                    percent = percent,
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
             }
-            Button(
-                onClick = {
-                    val message = encodeMessage(Message(id = "setTarget", fields = Target(0)))
-                        ?: "Hello World!"
-                    socketViewModel.webSocket?.send(message)
-                },
-                enabled = socketViewModel.status,
-                modifier = buttonModifier
+            Row(modifier = Modifier.padding(0.dp)) {
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                DataContent(
+                    text = "Motor 1",
+                    modifier = dataModifier.background(Color.Gray),
+                    innerPaddingValues = PaddingValues(16.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center
+                )
+                DataContent(
+                    text = "Motor 2",
+                    modifier = dataModifier.background(Color.Gray),
+                    innerPaddingValues = PaddingValues(16.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Row(modifier = Modifier.padding(0.dp)) {
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                DataContent(
+                    text = "M1: ${rpm_formatter.format(abs(10 * round(0.1 * ERPMToRPM(socketViewModel.values1.rpm ?: 0f))))} RPM",
+                    modifier = dataModifier,
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+                DataContent(
+                    text = "M2: ${rpm_formatter.format(abs(10 * round(0.1 * ERPMToRPM(socketViewModel.values2.rpm ?: 0f))))} RPM",
+                    modifier = dataModifier,
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+            }
+            Row(
+                modifier = Modifier.padding(0.dp)
             ) {
-                Text(text = "Stop")
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                DataContent(
+                    text = "M1: ${battery_formatter.format(socketViewModel.values1.temp_fet ?: 0f)} °C",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+                DataContent(
+                    text = "M2: ${battery_formatter.format(socketViewModel.values2.temp_fet ?: 0f)} °C",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+            }
+            Row(
+                modifier = Modifier.padding(0.dp)
+            ) {
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                DataContent(
+                    text = "M1: ${tach_formatter.format(ERevsToMiles( socketViewModel.values1.tachometer_abs ?: 0f))} Miles",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+                DataContent(
+                    text = "M2: ${tach_formatter.format(ERevsToMiles( socketViewModel.values2.tachometer_abs ?: 0f))} Miles",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+            }
+            Row(
+                modifier = Modifier.padding(0.dp)
+            ) {
+                val dataModifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                DataContent(
+                    text = "M1: ${rpm_formatter.format(abs(RPMToMPH(ERPMToRPM(socketViewModel.values1.rpm ?: 0f))))} MPH",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 0.dp, 16.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
+                DataContent(
+                    text = "M2: ${rpm_formatter.format(abs(RPMToMPH(ERPMToRPM(socketViewModel.values2.rpm ?: 0f))))} MPH",
+                    modifier = dataModifier,
+                    shape = RoundedCornerShape(0.dp, 0.dp, 16.dp, 0.dp),
+                    innerPaddingValues = PaddingValues(16.dp),
+                )
             }
         }
     }
--- app/src/main/java/com/example/longboardapp/SocketViewModel.kt
+++ app/src/main/java/com/example/longboardapp/SocketViewModel.kt
@@ -1,23 +1,11 @@
 package com.example.longboardapp
 
-import Data
-import Message
-import Target
 import Values
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.core.math.MathUtils.clamp
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import encodeMessage
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.WebSocket
@@ -38,7 +26,7 @@ class WebSocketListener(
     override fun onMessage(webSocket: WebSocket, text: String) {
         super.onMessage(webSocket, text)
         val msg = (parseMessage(text))!!
-        if ((msg.id ?: "") == "COMM_GET_VALUES") {
+        if ((msg.id) == "COMM_GET_VALUES") {
             val can = (msg.fields as Values).app_controller_id ?: 0
             if (can == 57) {
                 viewModel.updateValues1(msg.fields)
@@ -78,8 +66,12 @@ fun MPHToRPM(mph: Float): Float {
     return mph/(PI.toFloat()*0.09f*60f/1000f*0.621371f)
 }
 
-fun RPMTOERPM(rpm: Float): Float {
+fun RPMToERPM(rpm: Float): Float {
     return rpm*36f*7f/16f
+}
+
+fun ERevsToMiles(erevs: Float): Float {
+    return erevs*16f/36f/7f*(PI.toFloat()*0.09f/1000f*0.621371f)
 }
 
 class SocketViewModel : ViewModel() {
@@ -99,82 +91,27 @@ class SocketViewModel : ViewModel() {
         batteryPercent = percent
     }
 
-    val smoothing_factor = 0.01f
-
     fun updateValues1(values: Values){
         values1 = values
         val pv = ((values.v_in ?: 0f)-44.4f)/6f
-        batteryPercent = clamp(batteryPercent*(1-smoothing_factor)+pv*smoothing_factor, 0f, 1f)
+        batteryPercent = clamp(pv, 0f, 1f) //clamp(batteryPercent*(1-smoothing_factor)+pv*smoothing_factor, 0f, 1f)
     }
 
     fun updateValues2(values: Values){
         values2 = values
         val pv = ((values.v_in ?: 0f)-44.4f)/6f
-        batteryPercent = clamp(batteryPercent*(1-smoothing_factor)+pv*smoothing_factor, 0f, 1f)
+        batteryPercent = clamp(pv, 0f, 1f) //clamp(batteryPercent*(1-smoothing_factor)+pv*smoothing_factor, 0f, 1f)
     }
 
     fun updateStatus(_status: Boolean){
         status = _status
+        if(!status){
+            values1 = Values()
+            values2 = Values()
+            batteryPercent = 0f
+        }
     }
 
     val okHttpClient = OkHttpClient()
     var webSocket: WebSocket? = null
-
-    /*suspend fun DefaultClientWebSocketSession.writeMessages() {
-        while (true) {
-            val message2 = encodeMessage(Message(id="COMM_GET_VALUES", fields=Data())) ?: "Hello World!"
-            val message = encodeMessage(Message(id="setTarget", fields=Target(5000))) ?: "Hello World!"
-            //val message2 = encodeMessage(Message(id="getTarget", fields=Data())) ?: "Hello World!"
-            try {
-                send(message)
-                send(message2)
-            } catch (e: Exception) {
-                println("Error while sending: " + e.localizedMessage)
-                return
-            }
-            Thread.sleep(50)
-        }
-    }*/
-
-    /*suspend fun DefaultClientWebSocketSession.readMessages() {
-        try {
-            var count = 0
-            while(true){
-                val message = async { incoming.receive() }.await()
-                launch {
-                    message as Frame.Text
-                    val msg = (parseMessage(message.readText()))!!
-                    if ((msg.id ?: "") == "COMM_GET_VALUES") {
-
-                        val can = (msg.fields as Values).app_controller_id ?: 0
-                        if (can == 57) {
-                            updateValues1(msg.fields)
-                        } else if (can == 124) {
-                            updateValues2(msg.fields)
-                        }
-                    }
-                    println(count)
-                    count += 1
-                }
-            }
-        } catch (e: Exception) {
-            println("Error while receiving: " + e.localizedMessage)
-        }
-    }*/
-
-    /*fun runSocket() {
-        val client = HttpClient {
-            install(WebSockets)
-        }
-        viewModelScope.launch(Dispatchers.IO) {
-            client.webSocket("ws://192.168.0.27:8765") {
-                val messageOutputRoutine = launch { readMessages() }
-                val userInputRoutine = launch { writeMessages() }
-                userInputRoutine.join() // Wait for completion; either "exit" or error
-                messageOutputRoutine.cancelAndJoin()
-            }
-            client.close()
-            println("Connection closed. Goodbye!")
-        }
-    }*/
 }
